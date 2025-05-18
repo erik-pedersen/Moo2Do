5ff53d3c36508c4d40172935d069e901c6ef7fa4
Erik Pedersen <erik.pedersen138@gmail.com>
2025-05-18 14:59:47 +1000

    connected frontend to backend

--- public/scripts.js
+++ public/scripts.js
@@ -3,9 +3,40 @@ const newButton = document.getElementById("new");
 const editButton = document.getElementById("edit");
 const deleteButton = document.getElementById("delete");
 
+fetch("/api/mootodos")
+    .then(res => res.json())
+    .then(data => {
+        data.forEach(item => {
+            const el = document.createElement("div");
+            const title = document.createElement("p");
+            title.innerText = "Title: " + item.title;
+            const completed = document.createElement("p");
+            completed.innerText = "Completed: " + item.completed;
+            const text = document.createElement("p");
+            text.innerText = "Contents: " + item.text;
+
+            el.appendChild(title);
+            el.appendChild(completed);
+            el.appendChild(text);
+            el.id = item.id;
+            el.name = "todo-item";
+
+            todos.appendChild(el);
+        })
+    });
+
+
+
 newButton.addEventListener("click", () => {
     // create new child
     console.log("DEBUG: creating new todo item");
+    fetch("/api/mootodos", {method: "POST", body: JSON.stringify(
+        {
+            title: "New 2do",
+            text: "A new todo item",
+            completed: false
+        }
+    )});
 });
 
 editButton.addEventListener("click", () => {
@@ -17,3 +48,4 @@ deleteButton.addEventListener("click", () => {
     // create new child
     console.log("DEBUG: deleting todo item");
 });
+
--- server.js
+++ server.js
@@ -2,42 +2,72 @@ const express = require('express');
 const PORT = 3000
 const app = express();
 const logger = require('morgan');
-const mysql = require('mysql');
+// const mysql = require('mysql');
 const path = require("path");
 
-const con = mysql.createConnection({
-    host: "localhost",
-    user: "username",
-    password: "password"
-});
+// const con = mysql.createConnection({
+//     host: "localhost",
+//     user: "username",
+//     password: "password"
+// });
 
-con.connect( (err) => {
-    if (err) throw err;
-    console.log("Connected to MySQL database.\n");
-});
+// temporary in-memory database:
+
+data = {
+    todos: [
+        {
+            id: 1,
+            title: "First",
+            text: "This is a todo item",
+            completed: true
+        },
+        {
+            id: 2,
+            title: "Another todo",
+            text: "This is another todo item",
+            completed: false
+        },
+    ],
+    nextId: 3
+}
+
+// con.connect( (err) => {
+//     if (err) throw err;
+//     console.log("Connected to MySQL database.\n");
+// });
 
 app.use(logger("dev"));
 
 app.use(express.static("public"));
+
+app.get("/dashboard", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "dashboard.html"));
+});
 
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send("Something broke :(");
 });
 
-app.get("/mootodo", (req, res) => {
-    // get todo items
+app.get("/api/mootodos", (req, res) => {
+    res.status(200)
+    res.json(data.todos)
 });
 
-app.post("/mootodo", (req, res) => {
+app.post("/api/mootodos", (req, res) => {
+    res.status(200)
+    const item = req.body
+    console.log(item)
     // create new todo item
 });
 
-app.put("/mootodo", (req, res) => {
+app.put("/api/mootodos", (req, res) => {
+    res.status(200)
     // update todo item
 });
 
-app.delete("/mootodo", (req, res) => {
+app.delete("/api/mootodos", (req, res) => {
+    res.status(200)
     // delete a todo item
 });
 
